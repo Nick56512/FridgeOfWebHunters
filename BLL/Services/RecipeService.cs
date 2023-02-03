99bbecf632b34a3fcd3d9016cf13d8cbe678d996
Olaxander <mittsykh@ukr.net>
2023-02-03 14:28:54 +0200

    Create BLL

---
+++ BLL/Services/RecipeService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.DTO;
+using FridgeOfWebHunter.Interfaces;
+using FridgeOfWebHunter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class RecipeService : GenericService<Recipe, RecipeDto>
+    {
+        public RecipeService(IRepository<Recipe> repository) : base(repository)
+        {
+        }
+    }
+}
